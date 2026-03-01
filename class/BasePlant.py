34edbf166a3ba70df12a884cbb0719236ee0e332
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2026-03-01 20:19:40 +0000

    adding logic to change kwh used based on ramp rate

--- class/BasePlant.py
+++ class/BasePlant.py
@@ -54,11 +54,65 @@ class BasePlant():
         self.ramp_rate = ramp_rate
         self.maintain_days_ratio = maintain_days_ratio
         self.kwh_used = kwh_used
-        
+        self.changing_steps = 0
         # each time that a new plant is created then increment the id
         BasePlant.plant_id += 1 
 
+        # this variable must be increase for each day in order to request maintainement
+        self.days_without_maintainement = 0
+        self.kwh_used_before_shutting_down = 0
+
         print(f"Info: the plant of type {self.type} with id {self.id} has been created")
+
+    def set_initial_kwh(self, new_kwh):
+        if(new_kwh < self.min_operative_kwh or new_kwh > self.max_kwh):
+            print(f"Warning: {new_kwh} exceeds operative limits of this plant (id: {self.id})")
+            print(f"Operative limits: [{self.min_operative_kwh}, {self.max_kwh}]")
+            return
+        self.kwh_used = new_kwh
+
+    def verify_maintainement(self):
+        if self.days_without_maintainement >= self.maintain_days_ratio:
+            print(f"Info: plant with id {self.id} requires maintainement, shutting down...")
+            self.kwh_used_before_shutting_down = self.kwh_used
+            self.kwh_used = 0
+            
+    
+    def apply_maintainement(self):
+        print(f"Info: plant with id {self.id} is operative again...")
+        self.days_without_maintainement = 0
+        self.kwh_used = self.kwh_used_before_shutting_down
+        self.kwh_used_before_shutting_down = 0
+
+    def set_changing_steps(self, new_kwh):
+        if(new_kwh < self.min_operative_kwh or new_kwh > self.max_kwh):
+            print(f"Warning: {new_kwh} exceeds operative limits of this plant (id: {self.id})")
+            print(f"Operative limits: [{self.min_operative_kwh}, {self.max_kwh}]")
+            return
+        diff = new_kwh - self.kwh_used
+        acc = diff//self.ramp_rate
+        self.changing_steps = acc
+
+    def change_kwh_used_with_operating(self, new_kwh):
+        if(new_kwh < self.min_operative_kwh or new_kwh > self.max_kwh):
+            print(f"Warning: {new_kwh} exceeds operative limits of this plant (id: {self.id})")
+            print(f"Operative limits: [{self.min_operative_kwh}, {self.max_kwh}]")
+            return
+    
+        if self.kwh_used == new_kwh:
+            print("Info: plant was loaded successfully!")
+            self.set_changing_steps = 0
+            return
+        
+        if self.changing_steps == 0 and self.kwh_used != new_kwh:
+            self.set_changing_steps(new_kwh)
+             
+        s = f"Info: plant with id {self.id} will be setting its capacity "
+        t = f"in steps of {self.changing_steps} kwh until {new_kwh} kwh"
+        print(f"{s}{t}")
+        self.kwh_used += self.changing_steps
+
+
 
     def __str__(self):
         head = f"Plant: (type: {self.type}, id: {self.id}, price: {self.price}, kwh used: {self.kwh_used})\n"
