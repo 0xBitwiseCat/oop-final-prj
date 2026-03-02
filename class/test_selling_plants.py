65d8c853fdfd5c6910fea432315a7a0537b5d55b
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2026-03-02 20:11:07 +0000

    adding test for selling plants

---
+++ class/test_selling_plants.py
@@ -0,0 +1,20 @@
+from Store import Store
+from User import User
+from BasePlant import BasePlant
+from Plants import Coalplant
+from PlantParams import CoalPlantParams
+
+print("Probando la tienda :3")
+
+Store.generate_plants()
+Store.show_available_plants()
+
+user = User()
+
+plant0 = Coalplant(CoalPlantParams)
+user.plants.append(plant0)
+print(BasePlant.plant_id)
+Store.sell_plant(user, BasePlant.plant_id-1)
+print(user.plants)
+print(user.balance)
+
